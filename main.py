f5f92afec82f34ad0c4939f5c8f042090c8ee95c
Caio Ueno <caioluiggy@hotmail.com>
2022-04-20 17:01:10 -0300

    configure logging

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+import logging
+import logging.config
+
 from fastapi import FastAPI
+
 from api.v1.api import api_router
+
+logging.config.fileConfig("logging.conf")
+
+logger = logging.getLogger(__name__)
 
 app = FastAPI(
     title="ML API", version="0.0.1", description="short description about this API."
 )
 
-app.include_router(api_router)
+app.include_router(api_router, prefix="/api/v1")
 
 
 # if __name__ == "__main__":
