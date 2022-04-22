4bebd78e3afa3a4bb73277340c8df80578dcb858
Caio Ueno <caioluiggy@hotmail.com>
2022-04-22 10:15:58 -0300

    define input/output of routes

--- api/v1/endpoints/tweets.py
+++ api/v1/endpoints/tweets.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, List
 
 from core import utils
 from core.schemas import tweet, user
@@ -7,80 +7,48 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-@router.get("/{tweet_id}", response_model=tweet.Tweet)
+@router.get("/{tweet_id}", response_model=tweet.Tweet, status_code=200)
 def get_tweet(tweet_id: str) -> Any:
-
-    fake_tweet = {
-        "id": "sdfasdf",
-        "author_id": "sdjfioa",
-        "tweeted_at": utils.time_now(),
-        "text": "asdfjhas @de @asdijf",
-        "hastags": ["@de", "@asdijf"],
-        "retweets": [],
-        "likes": [],
-    }
-
-    return fake_tweet
+    return NotImplementedError()
 
 
-@router.post("/{user_id}/tweet", response_model=tweet.Tweet)
+@router.get(
+    "/user/{user_id}",
+    response_model=List[tweet.Tweet],
+    status_code=200,
+)
+def get_user_tweets(user_id: str) -> Any:
+    return NotImplementedError()
+
+
+@router.get(
+    "/{user_id}/timeline",
+    response_model=List[tweet.Tweet],
+    status_code=200,
+)
+def get_user_timeline(user_id: str) -> Any:
+    return NotImplementedError()
+
+
+@router.post("/{user_id}/tweet", response_model=tweet.Tweet, status_code=201)
 def publish_tweet(user_id: str, new_tweet: tweet.NewTweet) -> Any:
-
-    new_tweet = {
-        "id": utils.generate_md5(user_id + new_tweet["text"] + utils.time_now()),
-        "author_id": user_id,
-        "tweeted_at": utils.time_now(),
-        "text": new_tweet["text"],
-        "hashtags": utils.extract_hashtags(new_tweet["text"]),
-        "retweets": [],
-        "likes": [],
-    }
-
-    return new_tweet
+    return NotImplementedError()
 
 
-@router.post("/{user_id}/retweet/{tweet_id}", response_model=tweet.ReferenceTweet)
+@router.post(
+    "/{user_id}/retweet/{tweet_id}",
+    response_model=tweet.ReferenceTweet,
+    status_code=201,
+)
 def retweet(user_id: str, tweet_id: str) -> Any:
-
-    ref_tweet = {
-        "id": utils.generate_md5(user_id + tweet_id + utils.time_now()),
-        "user_id": user_id,
-        "referenced_at": utils.time_now(),
-        "tweet_id": tweet_id,
-    }
-
-    return ref_tweet
+    return NotImplementedError()
 
 
-@router.post("/{user_id}/like/{tweet_id}", response_model=tweet.Tweet)
+@router.put("/{user_id}/like/{tweet_id}", response_model=tweet.Tweet, status_code=200)
 def like(user_id: str, tweet_id: str) -> Any:
-
-    updated_tweet = {
-        "id": "sdfasdf",
-        "author_id": "sdjfioa",
-        "tweeted_at": utils.time_now(),
-        "text": "asdfjhas @de @asdijf",
-        "hastags": ["@de", "@asdijf"],
-        "retweets": [],
-        "likes": [],
-    }
-
-    updated_tweet["likes"].append(user.LikeUser(id=user_id, liked_at=utils.time_now()))
-
-    return updated_tweet
+    return NotImplementedError()
 
 
-@router.delete("/{user_id}/{tweet_id}")
+@router.delete("/{user_id}/{tweet_id}", status_code=200)
 def delete_tweet(user_id: str, tweet_id: str) -> Any:
-
-    fake_tweet = {
-        "id": "sdfasdf",
-        "author_id": "sdjfioa",
-        "tweeted_at": utils.time_now(),
-        "text": "asdfjhas @de @asdijf",
-        "hastags": ["@de", "@asdijf"],
-        "retweets": [],
-        "likes": [],
-    }
-
-    return True
+    return NotImplementedError()
--- core/utils.py
+++ core/utils.py
@@ -1,10 +1,15 @@
 from datetime import datetime
 import hashlib
+import re
 
 
 def time_now() -> str:
-    return datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S %Z")
+    return datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S %z")
 
 
 def generate_md5(string: str):
     return hashlib.md5(string.encode()).hexdigest()
+
+
+def extract_hashtags(text: str):
+    return re.findall(r"#[a-zA-Z]+", text)
