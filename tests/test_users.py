eed5d844bf2ef5c63db07d73a7173265a9d217ae
Caio Ueno <caioluiggy@hotmail.com>
2022-05-09 20:34:49 -0300

    add unfollow test and use classes to organize tests better

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,129 +1,300 @@
+import random
+import pytest
 from core import utils
 from fastapi import testclient
 from main import app
 
-client = testclient.TestClient(app)
+
+# @pytest.fixture
+# def version():
+#     return "v1"
 
 
-def test_get_user():
-
-    # get testuser1
-    response = client.get(f"api/v1/users/{utils.generate_md5('testuser1')}")
-    doc = dict(response.json())
-
-    assert doc["id"] == utils.generate_md5("testuser1")
-    assert doc["username"] == "testuser1"
-    assert response.status_code == 200
-
-    # non existing id
-    response = client.get(f"api/v1/users/notexistingid")
-    doc = dict(response.json())
-
-    assert doc == {}
-    assert response.status_code == 404
+@pytest.fixture
+def client():
+    return testclient.TestClient(app)
 
 
-def test_create_user():
+@pytest.fixture
+def setup_db(client):
 
-    # try to create an user with invalid chars
-    response = client.post("api/v1/users/", json={"username": "caio!"})
-    doc = dict(response.json())
+    if client.indices.exists("users-index"):
+        client.indices.delete("users-index")
 
-    assert doc == {}
-    assert response.status_code == 400
+    users_configs = {
+        "mappings": {
+            "properties": {
+                "id": {"type": "keyword"},
+                "username": {"type": "keyword"},
+                "joined_at": {"type": "date", "format": "yyyy-MM-dd HH:mm:ss x"},
+                "follows": {
+                    "type": "nested",
+                    "properties": {
+                        "id": {"type": "keyword"},
+                        "followed_at": {
+                            "type": "date",
+                            "format": "yyyy-MM-dd HH:mm:ss x",
+                        },
+                    },
+                },
+                "followers": {
+                    "type": "nested",
+                    "properties": {
+                        "id": {"type": "keyword"},
+                        "followed_at": {
+                            "type": "date",
+                            "format": "yyyy-MM-dd HH:mm:ss x",
+                        },
+                    },
+                },
+            }
+        }
+    }
 
-    # create an user caioueno
-    response = client.post("api/v1/users/", json={"username": "caioueno"})
-    doc = dict(response.json())
+    client.indices.create(index="users-index", body=users_configs)
 
-    assert doc["id"] == utils.generate_md5("caioueno")
-    assert doc["username"] == "caioueno"
-    assert response.status_code == 201
-
-    # try to create the same user again
-    response = client.post("api/v1/users/", json={"username": "caioueno"})
-    doc = dict(response.json())
-
-    assert doc["id"] == utils.generate_md5("caioueno")
-    assert doc["username"] == "caioueno"
-    assert response.status_code == 409
-
-
-def test_delete_user():
-
-    # try to delete non existing user
-    response = client.delete(f"api/v1/users/notexistingid")
-    doc = dict(response.json())
-
-    assert doc == {}
-    assert response.status_code == 404
-
-    # delete an existing user
-    response = client.delete(f"api/v1/users/{utils.generate_md5('caioueno')}")
-    doc = dict(response.json())
-
-    assert doc["id"] == utils.generate_md5("caioueno")
-    assert doc["username"] == "caioueno"
-    assert response.status_code == 200
-
-
-def test_follow():
-
-    # user who wants to follow doesn't exists
-    response = client.put(
-        f"api/v1/users/nonexistingid/follow/{utils.generate_md5('testuser2')}"
+    # test users
+    testuser1 = "testuser1"
+    client.create(
+        "users-index",
+        id=utils.generate_md5(testuser1),
+        body={
+            "id": utils.generate_md5(testuser1),
+            "username": testuser1,
+            "joined_at": utils.time_now(),
+            "follows": [],
+            "followers": [],
+        },
     )
 
-    doc = dict(response.json())
-
-    assert doc == {}
-    assert response.status_code == 404
-
-    # user to follow doesn't exists
-    response = client.put(
-        f"api/v1/users/{utils.generate_md5('testuser1')}/follow/nonexistingid"
+    testuser2 = "testuser2"
+    client.create(
+        "users-index",
+        id=utils.generate_md5(testuser2),
+        body={
+            "id": utils.generate_md5(testuser2),
+            "username": testuser2,
+            "joined_at": utils.time_now(),
+            "follows": [],
+            "followers": [],
+        },
     )
 
-    doc = dict(response.json())
-
-    assert doc == {}
-    assert response.status_code == 404
-
-    # neither users exists
-    response = client.put(f"api/v1/users/nonexistingid/follow/nonexistingid2")
-
-    doc = dict(response.json())
-
-    assert doc == {}
-    assert response.status_code == 404
-
-    # expected
-    response = client.put(
-        f"api/v1/users/{utils.generate_md5('testuser1')}/follow/{utils.generate_md5('testuser2')}"
+    testuser3 = "testuser3"
+    client.create(
+        "users-index",
+        id=utils.generate_md5(testuser3),
+        body={
+            "id": utils.generate_md5(testuser3),
+            "username": testuser3,
+            "joined_at": utils.time_now(),
+            "follows": [
+                {
+                    "id": utils.generate_md5("testuser4"),
+                    "followed_at": "2022-05-09 19:07:03 -0300",
+                }
+            ],
+            "followers": [],
+        },
     )
-    testuser1 = dict(response.json())
 
-    follow_users_ids = [item["id"] for item in testuser1["follows"]]
+    testuser4 = "testuser4"
+    client.create(
+        "users-index",
+        id=utils.generate_md5(testuser4),
+        body={
+            "id": utils.generate_md5(testuser4),
+            "username": testuser4,
+            "joined_at": utils.time_now(),
+            "follows": [],
+            "followers": [
+                {
+                    "id": utils.generate_md5(testuser3),
+                    "followed_at": "2022-05-09 19:07:03 -0300",
+                }
+            ],
+        },
+    )
 
-    assert testuser1["id"] == utils.generate_md5("testuser1")
-    assert utils.generate_md5("testuser2") in follow_users_ids
-    assert response.status_code == 200
 
-    response = client.get(f"api/v1/users/{utils.generate_md5('testuser2')}")
-    testuser2 = dict(response.json())
-    followers_users_ids = [item["id"] for item in testuser2["followers"]]
+class TestGetUser:
+    def test_nonexistent_user(self, client):
 
-    assert utils.generate_md5("testuser1") in followers_users_ids
+        response = client.get(f"api/v1/users/nonexistent")
+        expected_user = dict(response.json())
+
+        assert expected_user == {}
+        assert response.status_code == 404
+
+    def test_expected(self, client):
+
+        testuser1_id = utils.generate_md5("testuser1")
+
+        # get testuser1
+        response = client.get(f"api/v1/users/{testuser1_id}")
+        expected_user = dict(response.json())
+
+        assert expected_user["id"] == testuser1_id
+        assert expected_user["username"] == "testuser1"
+        assert response.status_code == 200
 
 
-# def test_unfollow():
+class TestCreateUser:
+    def test_invalid_name(self, client):
 
-#     response = client.put(
-#         f"api/v1/users/{utils.generate_md5('testuser1')}/unfollow/{utils.generate_md5('testuser2')}"
-#     )
-#     testuser1 = dict(response.json())
-#     follow_users_ids = [item["id"] for item in testuser1["follows"]]
+        # username with invalid chars
+        invalid_username = "caio!@@#"
 
-#     assert testuser1["id"] == utils.generate_md5("testuser1")
-#     assert utils.generate_md5("testuser2") not in follow_users_ids
-#     assert response.status_code == 200
+        response = client.post("api/v1/users/", json={"username": invalid_username})
+        expected_user = dict(response.json())
+
+        assert expected_user == {}
+        assert response.status_code == 400
+
+    def test_expected(self, client):
+
+        username = "".join(random.sample("abcdefghijklmnopqrstuvxwyz", 5))
+        idd = utils.generate_md5(username)
+
+        response = client.post("api/v1/users/", json={"username": username})
+        expected_user = dict(response.json())
+
+        assert expected_user["id"] == idd
+        assert expected_user["username"] == username
+        assert response.status_code == 201
+
+    def test_existent_user(self, client):
+
+        username = "testuser1"
+        idd = utils.generate_md5(username)
+
+        response = client.post("api/v1/users/", json={"username": username})
+        expected_user = dict(response.json())
+
+        assert expected_user["id"] == idd
+        assert expected_user["username"] == username
+        assert response.status_code == 409
+
+
+# class TestDeleteUser:
+#     def test_nonexistent_user(self, client):
+
+#         response = client.delete(f"api/v1/users/nonexistent")
+#         doc = dict(response.json())
+
+#         assert doc == {}
+#         assert response.status_code == 404
+
+#     def test_standard_behaviour(self, client):
+#         response = client.delete(f"api/v1/users/{utils.generate_md5('caioueno')}")
+#         doc = dict(response.json())
+
+#         assert doc["id"] == utils.generate_md5("caioueno")
+#         assert doc["username"] == "caioueno"
+#         assert response.status_code == 200
+
+
+class TestFollow:
+    def test_nonexistent_follower(self, client):
+
+        testuser2_id = utils.generate_md5("testuser2")
+
+        response = client.put(f"api/v1/users/nonexistent/follow/{testuser2_id}")
+        expected_user = dict(response.json())
+
+        assert expected_user == {}
+        assert response.status_code == 404
+
+    def test_nonexistent_followed(self, client):
+
+        testuser1_id = utils.generate_md5("testuser1")
+
+        response = client.put(f"api/v1/users/{testuser1_id}/follow/nonexistent")
+        expected_user = dict(response.json())
+
+        assert expected_user["id"] == testuser1_id
+        assert response.status_code == 404
+
+    def test_neither_exist(self, client):
+
+        response = client.put(f"api/v1/users/nonexistent1/follow/nonexistent2")
+        expected_user = dict(response.json())
+
+        assert expected_user == {}
+        assert response.status_code == 404
+
+    def test_expected(self, client):
+
+        testuser1_id = utils.generate_md5("testuser1")
+        testuser2_id = utils.generate_md5("testuser2")
+
+        response = client.put(f"api/v1/users/{testuser1_id}/follow/{testuser2_id}")
+        expected_user = dict(response.json())
+
+        follow_users_ids = [item["id"] for item in expected_user["follows"]]
+
+        assert expected_user["id"] == testuser1_id
+        assert testuser2_id in follow_users_ids
+        assert response.status_code == 200
+
+        # check if testuser2 document was updated as well
+        response = client.get(f"api/v1/users/{testuser2_id}")
+        expected_user = dict(response.json())
+
+        followers_users_ids = [item["id"] for item in expected_user["followers"]]
+
+        assert testuser1_id in followers_users_ids
+
+
+class TestUnfollow:
+    def test_nonexistent_follower(self, client):
+
+        testuser4_id = utils.generate_md5("testuser4")
+
+        response = client.put(f"api/v1/users/nonexistent/unfollow/{testuser4_id}")
+        expected_user = dict(response.json())
+
+        assert expected_user == {}
+        assert response.status_code == 404
+
+    def test_nonexistent_followed(self, client):
+
+        testuser3_id = utils.generate_md5("testuser3")
+
+        response = client.put(f"api/v1/users/{testuser3_id}/unfollow/nonexistent")
+        expected_user = dict(response.json())
+
+        assert expected_user["id"] == testuser3_id
+        assert response.status_code == 404
+
+    def test_neither_exist(self, client):
+
+        response = client.put(f"api/v1/users/nonexistent1/unfollow/nonexistent2")
+        expected_user = dict(response.json())
+
+        assert expected_user == {}
+        assert response.status_code == 404
+
+    def test_expected(self, client):
+
+        testuser3_id = utils.generate_md5("testuser3")
+        testuser4_id = utils.generate_md5("testuser4")
+
+        response = client.put(f"api/v1/users/{testuser3_id}/unfollow/{testuser4_id}")
+        expected_user = dict(response.json())
+
+        print(expected_user)
+
+        follow_users_ids = [item["id"] for item in expected_user["follows"]]
+
+        assert expected_user["id"] == testuser3_id
+        assert testuser4_id not in follow_users_ids
+        assert response.status_code == 200
+
+        # check if testuser2 document was updated as well
+        response = client.get(f"api/v1/users/{testuser4_id}")
+        expected_user = dict(response.json())
+
+        followers_users_ids = [item["id"] for item in expected_user["followers"]]
+
+        assert testuser3_id not in followers_users_ids
