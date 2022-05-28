b14c4d821f49ee8b90924f4efc037033da05ef1e
Caio Ueno <caioluiggy@hotmail.com>
2022-05-28 15:34:22 -0300

    add range date filter to query

--- api/v1/endpoints/sentiment.py
+++ api/v1/endpoints/sentiment.py
@@ -1,77 +1,106 @@
+import logging
 from typing import Union
-from core.schemas import ml
-from fastapi import APIRouter, Response
+
 import db
+import ml
+from core import utils
+from core.schemas import mlio
+from fastapi import APIRouter, Response
+
+USERS_INDEX = utils.get_config("elasticsearch.indices", "users")
+TWEETS_INDEX = utils.get_config("elasticsearch.indices", "tweets")
+
+logger = logging.getLogger(__name__)
 
 router = APIRouter()
-USERS_INDEX = "users-index"
 
 
 @router.post(
     "/classify",
-    response_model=Union[ml.PredictedSentiment, ml.InInstance],
+    response_model=Union[mlio.PredictedSentiment, mlio.InInstance],
     status_code=200,
 )
-def classify_text(instance: ml.InInstance, response: Response):
+def classify_text(instance: mlio.InInstance, response: Response):
 
     if instance.text == "":
         response.status_code = 500
         return instance
 
-    out = ml.PredictedSentiment(text=instance.text, sentiment="positive")
+    clf = ml.Dummy()
+    pred_sentiment = clf.single_prediction(instance.text)
+
+    out = mlio.PredictedSentiment(
+        text=instance.text, sentiment=utils.sentiment_to_str(pred_sentiment)
+    )
 
     return out
 
 
 @router.post(
-    "/quantify/user/{user_id}", response_model=Union[ml.SentimentPrevalence, ml.Empty]
+    "/quantify/user/{user_id}",
+    response_model=Union[mlio.SentimentPrevalence, mlio.Empty],
 )
 def quantify_user(
     user_id: str, response: Response, date_from: str = None, date_to: str = None
 ):
-    print(date_from)
-    print(date_to)
+
     if not db.es.exists(index=USERS_INDEX, id=user_id):
         response.status_code = 404
         return {}
 
-    buckets = db.es.search(
-        index="tweets-index",
-        body={
-            "query": {"bool": {"filter": {"term": {"author_id": user_id}}}},
-            "aggs": {"sentiments": {"terms": {"field": "sentiment"}}},
+    query = {
+        "query": {
+            "bool": {
+                "filter": [
+                    {"term": {"author_id": user_id}},
+                ]
+            }
         },
-    )["aggregations"]["sentiments"]["buckets"]
+        "aggs": {"sentiments": {"terms": {"field": "sentiment"}}},
+    }
 
-    mapping_sentiment = {-1: "negative", 0: "neutral", 1: "positive"}
-    prevalence = {}
+    range_clause = utils.date_filter(date_from, date_to)
+
+    if range_clause is not None:
+        query["query"]["bool"]["filter"].append(range_clause)
+
+    buckets = db.es.search(index=TWEETS_INDEX, body=query)["aggregations"][
+        "sentiments"
+    ]["buckets"]
+
+    prevalence = {"negative": 0, "neutral": 0, "positive": 0}
+
     for b in buckets:
-        prevalence[mapping_sentiment[b["key"]]] = b["doc_count"]
-
-    # prevalence = {"negative": 0.0, "neutral": 0.0, "positive": 1.0}
+        prevalence[utils.sentiment_to_str(b["key"])] = b["doc_count"]
 
     return prevalence
 
 
 @router.post(
     "/quantify/hashtag/{hashtag}",
-    response_model=Union[ml.SentimentPrevalence, ml.Empty],
+    response_model=Union[mlio.SentimentPrevalence, mlio.Empty],
 )
 def quantify_hashtag(
     hashtag: str, response: Response, date_from: str = None, date_to: str = None
 ):
 
-    buckets = db.es.search(
-        index="tweets-index",
-        body={
-            "query": {"bool": {"filter": {"term": {"hashtags": "#" + hashtag}}}},
-            "aggs": {"sentiments": {"terms": {"field": "sentiment"}}},
-        },
-    )["aggregations"]["sentiments"]["buckets"]
+    query = {
+        "query": {"bool": {"filter": [{"term": {"hashtags": "#" + hashtag}}]}},
+        "aggs": {"sentiments": {"terms": {"field": "sentiment"}}},
+    }
 
-    mapping_sentiment = {-1: "negative", 0: "neutral", 1: "positive"}
-    prevalence = {}
+    range_clause = utils.date_filter(date_from, date_to)
+
+    if range_clause is not None:
+        query["query"]["bool"]["filter"].append(range_clause)
+
+    buckets = db.es.search(index=TWEETS_INDEX, body=query,)["aggregations"][
+        "sentiments"
+    ]["buckets"]
+
+    prevalence = {"negative": 0, "neutral": 0, "positive": 0}
+
     for b in buckets:
-        prevalence[mapping_sentiment[b["key"]]] = b["doc_count"]
-    print(prevalence)
+        prevalence[utils.sentiment_to_str(b["key"])] = b["doc_count"]
+
     return prevalence
